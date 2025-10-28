b348a6875fae39d564caeda713429f46c5471d3d
ddale314 <dylandale999@gmail.com>
2025-10-28 13:50:56 -0700

    note and frequency display

--- components/audioCanvas.js
+++ components/audioCanvas.js
@@ -5,18 +5,67 @@ import { fft } from '../utils/fft';
 let mediaStream;
 let audioContext;
 let analyser;
-let timeDomainData;
-let frequencyData = [];
+
 let fftSize = 8192;
-let bufferLength = fftSize/2;
+let bufferLength = fftSize / 2;
+
+const INTERVAL = Math.pow(2, 1/12)
+const NOTES = ['A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A']
 
 export function setMediaStream(stream) {
     mediaStream = stream;
 }
 
-export default function AudioCanvas({ type, width, height }) {
+function getFrequencyData(analyser) {
+    let timeDomainData = new Float32Array(bufferLength);
+    analyser.getFloatTimeDomainData(timeDomainData);
+    let frequencyData = [];
+    for (let i = 0; i < bufferLength; i++) {
+        frequencyData[i] = new Complex(timeDomainData[i], 0);
+    }
+    fft(frequencyData, false);
+    return frequencyData;
+}
+
+function initAnalyser(audioContext) {
+    const source = audioContext.createMediaStreamSource(mediaStream);
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = fftSize;
+    source.connect(analyser);
+}
+
+function getFrequencyAndNote(frequencyData, sampleRate) {
+    let maxMagnitude = 0;
+    let maxFrequency = 0;
+    for (let i = 0; i < bufferLength; i++) {
+        let magnitude = frequencyData[i].magnitude();
+        if (magnitude > maxMagnitude) {
+            maxMagnitude = magnitude;
+            maxFrequency = i;
+        }
+    }
+    let realFrequency = indexToFrequency(maxFrequency, sampleRate);
+    
+    const A = 440;
+    let difference = Math.log(realFrequency / A) / Math.log(INTERVAL);
+    difference = Math.trunc(difference);
+    let note = (Math.abs(difference) % 12) * Math.sign(difference);
+    if (Math.sign(note) == -1) {
+        note = 12 + note;
+    }
+
+    return [NOTES[note - 1], realFrequency] // 0 index
+}
+
+function indexToFrequency(idx, sampleRate) {
+    return Math.round(idx * (sampleRate / (fftSize / 2)));
+}
+
+export default function AudioCanvas({ type, width, height, data=null }) {
     const canvasRef = useRef(null);
     const [context, setContext] = useState(null);
+    const [note, setNote] = useState(-1);
+    const [frequency, setFrequency] = useState(-1);
 
     const draw = useCallback(() => {
         if (mediaStream) {
@@ -24,18 +73,17 @@ export default function AudioCanvas({ type, width, height }) {
                 audioContext = new AudioContext();
             }
             if (!analyser) {
-                const source = audioContext.createMediaStreamSource(mediaStream);
-                analyser = audioContext.createAnalyser();
-                analyser.fftSize = fftSize;
-                source.connect(analyser);
-                timeDomainData = new Float32Array(bufferLength);
+                initAnalyser(audioContext);
             }
-            
-            analyser.getFloatTimeDomainData(timeDomainData);
-            for (let i = 0; i < bufferLength; i++) {
-                frequencyData[i] = new Complex(timeDomainData[i], 0);
+
+            let frequencyData;
+
+            if (data === null) {
+                 frequencyData = getFrequencyData(analyser);
             }
-            fft(frequencyData, false);
+            else {
+                frequencyData = data
+            }
 
             context.fillStyle = 'rgb(255, 255, 255)';
             context.fillRect(0, 0, width, height);
@@ -49,16 +97,20 @@ export default function AudioCanvas({ type, width, height }) {
             let x = 0;
 
             for (let i = 0; i < (bufferLength / 4); i++) {
-                let y = -frequencyData[i].magnitude()+height;
+                let magnitude = frequencyData[i].magnitude();
+                let y = -magnitude + height;
                 if (i === 0) {
                     context.moveTo(x, y);
                 }
                 else {
                     context.lineTo(x, y);
                 }
-
                 x += increment;
             }
+
+            let [currentNote, currentFrequency] = getFrequencyAndNote(frequencyData, audioContext.sampleRate);
+            setNote(currentNote);
+            setFrequency(currentFrequency);
         }
         context.stroke();
     }, [context, height, width]);
@@ -68,7 +120,8 @@ export default function AudioCanvas({ type, width, height }) {
             const ctx = canvasRef.current.getContext('2d');
             setContext(ctx);
         }
-    }, []);
+    }, [])
+    
 
     useEffect(() => {
         let animationFrameId;
@@ -86,7 +139,11 @@ export default function AudioCanvas({ type, width, height }) {
         }
     }, [draw, context]);
 
+
     return (
-        <canvas ref={canvasRef} width={width} height={height}></canvas>
+        <>
+            <canvas ref={canvasRef} width={width} height={height}></canvas>
+            <div>Frequency: {frequency}, Note: {note}</div>
+        </>
     );
 }
--- pages/record.js
+++ pages/record.js
@@ -1,9 +1,33 @@
 import Head from "next/head";
 import utilStyles from '../styles/utils.module.css';
 import Layout from "../components/layout";
+import AudioCanvas, { setMediaStream } from '../components/audioCanvas';
+import { Complex } from "../utils/complex";
+import { useEffect } from 'react';
+
+let testData = []
+for (let i = 0; i < 4096; i++) {
+    let i = Math.random() * 60 - 30
+    let j = Math.random() * 60 - 30
+    testData.push(new Complex(i, j));
+}
 
 // record, display, and edit audio
 export default function Recording() {
+    useEffect(() => {
+        if (navigator.mediaDevices.getUserMedia) {
+            const constraints = { audio: true };
+            let onSuccess = (stream) => {
+                setMediaStream(stream);
+            }
+    
+            let onError = () => {
+                console.log('Error: ' + err);
+            }
+    
+            navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+        }
+    }, []);
     return (
         <Layout>
             <Head>
@@ -11,7 +35,10 @@ export default function Recording() {
             </Head>
             <section className = {utilStyles.titleLarge + ' ' + utilStyles.textGradient}>
                 <p>Record</p>
+                
             </section>
+            <AudioCanvas type='dynamic' width={1000} height={100}/>
+            <AudioCanvas type='static' width={1000} height={250} data={testData}/>
         </Layout>
     )
 }
