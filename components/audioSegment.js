fd71a6f97d0b6025ee32a1e671dcc9a5b3900c4a
ddale314 <dylandale999@gmail.com>
2025-12-05 17:30:29 -0800

    work on drag audiosegment

--- components/audioSegment.js
+++ components/audioSegment.js
@@ -31,6 +31,11 @@ export class AudioSegment {
 		let right = new AudioSegment(nextID + 1, this.data, this.start + pos, this.stop, this.track, pos);
 		return [left, right];
 	}
+
+	getX(amount) {
+		this.start += amount;
+		this.stop += amount;
+	}
 	
 	stopAudio() {
 		if (this.source) {
@@ -40,10 +45,51 @@ export class AudioSegment {
 }
 
 export function AudioSegmentComponent({ ctx, audioSegment, size }) {
+	const [dragging, setDragging] = useState(false);
+	const [relativePos, setRelativePos] = useState(0);
+	//const ref = useRef();
+
+	useEffect(() => {
+		document.addEventListener("mousemove", handleMouseMove);
+		document.addEventListener("mouseup", handleMouseUp);
+
+		return () => {
+			document.removeEventListener("mousemove", handleMouseMove);
+			document.removeEventListener("mouseup", handleMouseUp);
+		};
+	}, [dragging]);
+
+	function handleMouseMove(e) {
+		if (!dragging) return;
+		
+		let newPos = e.pageX - relativePos;
+		if (newPos >= 0) {
+			audioSegment.translateX(newPos - audioSegment.start);
+			setRelativePos(newPos);
+		}
+
+		e.stopPropagation();
+		e.preventDefault();
+	}
+
+	function handleMouseUp(e) {
+		setDragging(false);
+		e.stopPropagation();
+		e.preventDefault();
+	}
+	
+	function handleMouseDown(e) {
+		let pos = e.target.getBoundingClientRect();
+		setRelativePos(e.pageX - pos.left);
+		setDragging(true);
+		e.stopPropagation();
+		e.preventDefault();
+	}
+
 	return (
 		<>
 			<span>
-				<button className={styles.audioSegment} style={{position: "absolute", padding: "0px", left: `${audioSegment.start * size}px`, width: `${(audioSegment.stop - audioSegment.start) * size}px`}}>
+				<button onMouseMove={handleMouseMove} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className={styles.audioSegment} style={{position: "absolute", padding: "0px", left: `${audioSegment.start * size}px`, width: `${(audioSegment.stop - audioSegment.start) * size}px`}}>
 					{(audioSegment.stop - audioSegment.start).toFixed(1)} seconds
 				</button>
 			</span>
